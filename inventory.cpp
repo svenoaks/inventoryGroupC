0075b517e0677b170f2491285284749ae154c465
Steve <svenoaks@gmail.com>
2013-11-02 10:59:49 -0400

    read file and sorting

--- inventory.cpp
+++ inventory.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
-enum userSelection { CHECK, SELL, PRINT, QUIT};
+enum userSelection { CHECK, SELL, PRINT, TEST, QUIT };
 
+void testVecs();
 void printReport();
 double getTotalValue();
 int getTotalItems();
 void sortVecs();
-void readFileIntoVecs();
+bool readFileIntoVecs();
 bool isItemInStore(string);
 string sellItem(string);
 userSelection getSelectionFromUser();
@@ -28,8 +30,12 @@ vector<double> sellingPrices;
 //main() by Steve Myers
 int main()
 {
+	if (! readFileIntoVecs()) 
+	{
+		system("pause");
+		return 1;
+	}
 
-	readFileIntoVecs();
 	sortVecs();
 
 	while (true)
@@ -53,6 +59,8 @@ int main()
 		case PRINT:
 			printReport();
 			break;
+		case TEST:
+			testVecs();
 		case QUIT:
 		default:
 			wantsToQuit = true;
@@ -64,20 +72,92 @@ int main()
 	return 0;
 }
 
-//getItemFromUser() and sortVecs() by Steve Myers
+//getItemFromUser() readFileIntoVects() and sortVecs() by Steve Myers
 
 string getItemFromUser(userSelection selection)
 {
-	return " ";
+	switch (selection)
+	{
+	case CHECK:
+		cout << "Enter name of item to check inventory: ";
+		break;
+	case SELL:
+		cout <<  "Enter name of item to sell: ";
+		break;
+	}
+
+	string input;
+	getline(cin, input); 
+	return input;
+}
+bool readFileIntoVecs()
+{
+	ifstream inFile;
+	
+	//place intial.txt in the project folder
+	inFile.open("initial.txt");
+	
+	if (!inFile)
+	{
+		cout << "Invalid input.";
+		inFile.close();
+		return false;
+	}
+	
+	while (inFile)
+	{
+		int itemId;
+		string itemName;
+		int pOrdered;
+		double manufPrice, sellingPrice;
+
+		inFile >> itemId;
+		inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+		getline(inFile, itemName);
+		inFile >> pOrdered >> manufPrice >> sellingPrice;
+		inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		itemIds.push_back(itemId);
+		itemNames.push_back(itemName);
+		pOrdereds.push_back(pOrdered);
+		pInStores.push_back(pOrdered);
+		pSolds.push_back(0);
+		manufPrices.push_back(manufPrice);
+		sellingPrices.push_back(sellingPrice);
+		
+	}
+
+	inFile.close();
+	return true;
 }
 void sortVecs()
 {
+    int min;
+ 
+    for (int i = 0; i < itemIds.size(); i++)
+    {
+        min = i;
+
+        for (int j = i + 1; j < itemIds.size(); j++)
+            if (itemNames[j] < itemNames[min])
+                min = j;
+        
+		swap(itemNames[i], itemNames[min]);
+		swap(itemIds[i], itemIds[min]);
+		swap(pOrdereds[i], pOrdereds[min]);
+		swap(pInStores[i], pInStores[min]);
+		swap(pSolds[i], pSolds[min]);
+		swap(manufPrices[i], manufPrices[min]);
+		swap(sellingPrices[i], sellingPrices[min]);
+    }
 }
-
-
-//readFileIntoVecs() by ...
-void readFileIntoVecs()
+void testVecs()
 {
+	for (int i = 0; i < itemIds.size(); i++)
+	{
+		cout << itemIds[i] << endl << itemNames[i] << endl << pOrdereds[i] 
+		<< endl << pSolds[i] << endl << manufPrices[i] << endl << sellingPrices[i] << endl;
+	}
 }
 
 
@@ -116,5 +196,5 @@ void showMenu()
 }
 userSelection getSelectionFromUser()
 {
-	return QUIT;
+	return TEST;
 }
