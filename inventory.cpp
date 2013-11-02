46eeaeed4470c1924aa88f0370a22cea0b14f527
Steve <svenoaks@gmail.com>
2013-11-02 11:54:37 -0400

    added fileinput() and noOfRows()

--- inventory.cpp
+++ inventory.cpp
@@ -12,12 +12,14 @@ void printReport();
 double getTotalValue();
 int getTotalItems();
 void sortVecs();
-bool readFileIntoVecs();
+void readFileIntoVecs();
 bool isItemInStore(string);
 string sellItem(string);
 userSelection getSelectionFromUser();
 string getItemFromUser(userSelection);
 void showMenu();
+void fileInput(int);
+int noOfRows();
 
 vector<int> itemIds;
 vector<string> itemNames;
@@ -30,12 +32,8 @@ vector<double> sellingPrices;
 //main() by Steve Myers
 int main()
 {
-	if (! readFileIntoVecs()) 
-	{
-		system("pause");
-		return 1;
-	}
-
+	readFileIntoVecs();
+	
 	sortVecs();
 
 	while (true)
@@ -72,7 +70,7 @@ int main()
 	return 0;
 }
 
-//getItemFromUser() readFileIntoVects() and sortVecs() by Steve Myers
+//getItemFromUser() readFileIntoVecs() and sortVecs() by Steve Myers
 
 string getItemFromUser(userSelection selection)
 {
@@ -90,58 +88,23 @@ string getItemFromUser(userSelection selection)
 	getline(cin, input); 
 	return input;
 }
-bool readFileIntoVecs()
+void readFileIntoVecs()
 {
-	ifstream inFile;
-	
-	//place intial.txt in the project folder
-	inFile.open("initial.txt");
-	
-	if (!inFile)
-	{
-		cout << "Invalid input.";
-		inFile.close();
-		return false;
-	}
-	
-	while (inFile)
-	{
-		int itemId;
-		string itemName;
-		int pOrdered;
-		double manufPrice, sellingPrice;
-
-		inFile >> itemId;
-		inFile.ignore(numeric_limits<streamsize>::max(), '\n');
-		getline(inFile, itemName);
-		inFile >> pOrdered >> manufPrice >> sellingPrice;
-		inFile.ignore(numeric_limits<streamsize>::max(), '\n');
-
-		itemIds.push_back(itemId);
-		itemNames.push_back(itemName);
-		pOrdereds.push_back(pOrdered);
-		pInStores.push_back(pOrdered);
-		pSolds.push_back(0);
-		manufPrices.push_back(manufPrice);
-		sellingPrices.push_back(sellingPrice);
-		
-	}
-
-	inFile.close();
-	return true;
+	fileInput(noOfRows());
 }
+
 void sortVecs()
 {
-    int min;
- 
-    for (int i = 0; i < itemIds.size(); i++)
-    {
-        min = i;
+	int min;
 
-        for (int j = i + 1; j < itemIds.size(); j++)
-            if (itemNames[j] < itemNames[min])
-                min = j;
-        
+	for (unsigned i = 0; i < itemIds.size(); i++)
+	{
+		min = i;
+
+		for (unsigned j = i + 1; j < itemIds.size(); j++)
+			if (itemNames[j] < itemNames[min])
+				min = j;
+
 		swap(itemNames[i], itemNames[min]);
 		swap(itemIds[i], itemIds[min]);
 		swap(pOrdereds[i], pOrdereds[min]);
@@ -149,18 +112,85 @@ void sortVecs()
 		swap(pSolds[i], pSolds[min]);
 		swap(manufPrices[i], manufPrices[min]);
 		swap(sellingPrices[i], sellingPrices[min]);
-    }
+	}
 }
 void testVecs()
 {
-	for (int i = 0; i < itemIds.size(); i++)
+	for (unsigned i = 0; i < itemIds.size(); i++)
 	{
 		cout << itemIds[i] << endl << itemNames[i] << endl << pOrdereds[i] 
 		<< endl << pSolds[i] << endl << manufPrices[i] << endl << sellingPrices[i] << endl;
 	}
 }
 
+//isItemIntStore() and sellItem() by Steve Myers
+bool isItemInStore(string userInput)
+{
+	return false;
+}
+string sellItem(string userInput)
+{
+	return "";
+}
 
+
+//counts lines in textfile: Jaime Woodbury
+int noOfRows()                                         
+{
+	int numberOfRows = 0;
+	string line;
+	ifstream myfile("initial.txt");
+
+	while (getline(myfile, line))
+		numberOfRows++;
+
+	return numberOfRows;
+}
+
+
+//inputs file data into vectors: Jaime Woodbury
+void fileInput(int noOfRows)
+{
+	ifstream inf("initial.txt");
+	string word;
+	int item, order; 
+	double manPrice, sellPrice;
+	char ch;
+	
+	for (unsigned i=0; i<noOfRows/3; i++)
+
+	{      
+		inf>>item;
+		itemIds.push_back(item);
+
+		inf.get(ch); 
+
+		getline(inf, word);
+		itemNames.push_back(word);
+
+		inf>>order;
+		pOrdereds.push_back(order);
+
+		inf>>manPrice;
+		manufPrices.push_back(manPrice);
+
+		inf>>sellPrice;
+		sellingPrices.push_back(sellPrice);
+	}
+
+	for (unsigned i=0; i<itemIds.size(); i++)
+	{
+		//intilizes vector pSolds to 0
+		int sold = 0;
+		pSolds.push_back(sold);
+
+		//adds data to pInStores vector
+		int inStore;
+		inStore = pOrdereds.at(i)-pSolds.at(i);
+		pInStores.push_back(inStore);
+
+	}
+}      
 
 //printReport(), getTotalValue() and getTotalItems() by...
 void printReport()
@@ -176,19 +206,6 @@ int getTotalItems()
 }
 
 
-
-//isItemIntStore() and sellItem() by ....
-bool isItemInStore(string userInput)
-{
-	return false;
-}
-string sellItem(string userInput)
-{
-	return "";
-}
-
-
-
 //showMenu() and getSelectionFromUser by ....
 void showMenu()
 {
@@ -198,3 +215,46 @@ userSelection getSelectionFromUser()
 {
 	return TEST;
 }
+
+/*
+bool readFileIntoVecs()
+{
+ifstream inFile;
+
+//place intial.txt in the project folder
+inFile.open("initial.txt");
+
+if (!inFile)
+{
+cout << "Invalid input.";
+inFile.close();
+return false;
+}
+
+while (inFile)
+{
+int itemId;
+string itemName;
+int pOrdered;
+double manufPrice, sellingPrice;
+
+inFile >> itemId;
+inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+getline(inFile, itemName);
+inFile >> pOrdered >> manufPrice >> sellingPrice;
+inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+
+itemIds.push_back(itemId);
+itemNames.push_back(itemName);
+pOrdereds.push_back(pOrdered);
+pInStores.push_back(pOrdered);
+pSolds.push_back(0);
+manufPrices.push_back(manufPrice);
+sellingPrices.push_back(sellingPrice);
+
+}
+
+inFile.close();
+return true;
+}
+*/
